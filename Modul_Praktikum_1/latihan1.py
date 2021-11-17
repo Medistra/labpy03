fc577b8797869c449c17d583907ecb65d598b019
Medistra <medistra37@gmail.com>
2021-11-16 18:00:59 -0800

    Menambahkan folder

---
+++ Modul_Praktikum_1/latihan1.py
@@ -0,0 +1,9 @@
+print ("Program sederhana dengan input 2 buah bilangan")
+
+a = int(input("Masukan bilangan pertama: "))
+b = int(input("Masukan bilangan kedua: "))
+
+if a > b:
+    print("Bilangan pertama adalah bilangan terbesar = %s" % a)
+else:
+    print("Bilangan kedua adalah bilangan terbesar = %s" % b)
